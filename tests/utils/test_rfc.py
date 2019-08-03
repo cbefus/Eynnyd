f1b81d22386cbbb964798dc020b34e59e53b91dd
cbefus <crbefus@gmail.com>
2019-08-03 17:50:43 -0500

    Added some tests for the cookie rfc regex.

--- tests/utils/test_rfc.py
+++ tests/utils/test_rfc.py
@@ -657,6 +657,268 @@ class TestDomains(unittest.TestCase):
                 msg="Bad domain '{l}' matched regex".format(l=bad_example))
 
 
+class TestRFCCookie(unittest.TestCase):
+
+    def test_cookie_octet_inclusive(self):
+        expression = re.compile(rfc._COOKIE_OCTET_REGEX)
+        allowed_values = [
+            "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", "<", "=",
+            ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+            "X", "Y", "Z", "[", "]", "^", "_", "`", "a", "b", "c", "d", "e",
+            "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+            "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"
+        ]
+        for allowed_value in allowed_values:
+            self.assertTrue(
+                bool(expression.fullmatch(allowed_value)),
+                msg="Allowed Octet '{o}' not matched by regex".format(o=allowed_value))
+
+    def test_cookie_octet_exclusion(self):
+        expression = re.compile(rfc._COOKIE_OCTET_REGEX)
+        unallowed_values = [
+            "\x00", "\x01", "\x02", "\x03", "\x04", "\x06", "\x07", "\x08",
+            "\x09", "\x0A", "\t", "\x0C", "\x0D", "\x0E", "\x0F", "\x10", "\x11",
+            "\x12", "\x13", "\x14", "\x15", "\x16", "\x17", "\x18", "\x19",
+            "\x1A", "\x1B", "\x1C", "\x1D", "\x1E", "\x1F", " ", "\"", ",",
+            ";", "\\", "\x7F"
+        ]
+        for unallowed_value in unallowed_values:
+            self.assertFalse(
+                bool(expression.fullmatch(unallowed_value)),
+                msg="Unallowed Octedt '{u}' matched by regex".format(u=unallowed_value))
+
+    def test_cookie_value_inclusion(self):
+        expression = re.compile(rfc._COOKIE_VALUE_REGEX)
+        good_examples = [
+            "a_cookie_value",
+            "\"quoted-(cookie-value)\"",
+            "#big_$$_yo!",
+            "&that's-how-we-do",
+            "some-symbols[*+./:<>=?@^_`{|}]~"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="valid cookie value '{v}' did not match regex".format(v=good_example))
+
+    def test_cookie_value_exclusion(self):
+        expression = re.compile(rfc._COOKIE_VALUE_REGEX)
+        bad_examples = [
+            "a bad cookie value",
+            "\"a-\"bad\"-cookie-value\"",
+            "a-cookie-value,bad",
+            "\"a-cookie;bad-value\"",
+            "cookie-value:\tbad",
+            "is-that-a-backslash?\\yep"
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad cookie value '{v}' was not rejected by regex".format(v=bad_example))
+
+    def test_expires_av_inclusive(self):
+        expression = re.compile(rfc._EXPIRES_AV_REGEX)
+        good_examples = [
+            "Expires=Mon Jan  3 03:21:45 2045",
+            "Expires=Thu Oct 31 20:34:22 1982",
+            "Expires=Monday, 31-Oct-82 03:21:45 GMT",
+            "Expires=Thursday, 01-Feb-45 20:34:22 GMT",
+            "Expires=Mon, 31 Oct 1982 03:21:45 GMT",
+            "Expires=Thu, 01 Feb 2045 20:34:22 GMT"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good Expires AV '{e}' did not match regex".format(e=good_example))
+
+    def test_expires_av_exclusive(self):
+        expression = re.compile(rfc._EXPIRES_AV_REGEX)
+        bad_examples = [
+            "Exp=Mon Jan  3 03:21:45 2045",
+            "expires=Mon Jan  3 03:21:45 2045",
+            "Expires= Thu Oct 31 20:34:22 1982",
+            "Expires=Mon Jan 3 03:21:45 2045",
+            "Expires=Mon, Jan  3 03:21:45 2045",
+            "Expires=Mon Jan  3 03:1:45 2045",
+            "Expires=Mon Jan  3 03:01:45 45",
+            "Expires=Mon Jan  3 03:01:45 2045 GMT",
+            "Expires=Thu Oct  31 20:34:22 1982",
+            "Expires=Monday 31-Oct-82 03:21:45 GMT",
+            "Expires=Monday, 31-Oct-82 03:21:45",
+            "Expires=Monday, 31-Oct-1982 03:21:45 GMT",
+            "Expires=Monday, 6-Oct-82 03:21:45 GMT",
+            "Expires=Mon 31 Oct 1982 03:21:45 GMT",
+            "Expires=Mon, 31 Oct 1982 03:21:45",
+            "Expires=Mon, 31 Oct 82 03:21:45 GMT",
+            "Expires=Mon, 6 Oct 1982 03:21:45 GMT"
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Expires AV '{e}' matched regex".format(e=bad_example))
+
+    def test_max_age_av_inclusive(self):
+        expression = re.compile(rfc._MAX_AGE_AV_REGEX)
+        good_examples = [
+            "Max-Age=1234",
+            "Max-Age=1"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good Max-Age AV '{m}' not matched by regex".format(m=good_example))
+
+    def test_max_age_av_exclusive(self):
+        expression = re.compile(rfc._MAX_AGE_AV_REGEX)
+        bad_examples = [
+            "Max-age=1234",
+            "Max-Age=0",
+            "Max-Age=a",
+            "Max=1",
+            "Max-Age="
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Max-Age AV '{m}' matched by regex".format(m=bad_example))
+
+    def test_domain_av_inclusive(self):
+        expression = re.compile(rfc._DOMAIN_AV_REGEX)
+        good_examples = [
+            "Domain=a",
+            "Domain=a-7",
+            "Domain=foo-77.a-7.aa",
+            "Domain=localhost",
+            "Domain=foo.bar.com",
+            "Domain=0.0.0.0",
+            "Domain=127.0.0.1",
+            "Domain=250.250.250.250",
+            "Domain=255.255.255.255",
+            "Domain=249.249.249.249",
+            "Domain=200.200.200.200",
+            "Domain=199.199.199.199",
+            "Domain=100.100.100.100",
+            "Domain=2001:0db8:0000:0000:0000:ff00:0042:8329",
+            "Domain=2001:db8:0:0:0:ff00:42:8329",
+            "Domain=2001:db8::ff00:42:8329",
+            "Domain=0000:0000:0000:0000:0000:0000:0000:0001",
+            "Domain=::1"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good Domain AV '{m}' not matched by regex".format(m=good_example))
+
+    def test_domain_av_exclusive(self):
+        expression = re.compile(rfc._DOMAIN_AV_REGEX)
+        bad_examples = [
+            "domain=a",
+            "Domain= a-7",
+            "Dom=foo-77.a-7.aa",
+            "Domain=127.1.1",
+            "Domain=127.a.1.1",
+            "Domain=2001:0dg8:0000:0000:0000:ff00:0042:8329",
+            "Domain=7",
+            "Domain=-",
+            "Domain=a.-aa",
+            "Domain=a.7aa",
+            "Domain=a-",
+            "Domain=a.",
+            "Domain=.a",
+            "Domain="
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Domain AV '{m}' matched by regex".format(m=bad_example))
+
+    def test_path_av_inclusive(self):
+        expression = re.compile(rfc._PATH_AV_REGEX)
+        good_examples = [
+            "Path=~some/random/path.foo"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good Path AV '{m}' not matched by regex".format(m=good_example))
+
+    def test_path_av_exclusive(self):
+        expression = re.compile(rfc._PATH_AV_REGEX)
+        bad_examples = [
+            "Path=",
+            "Path=\t~some/random/path.foo",
+            "path=~some/random/path.foo",
+            "Path=~some/random/path.foo;",
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Path AV '{m}' matched by regex".format(m=bad_example))
+
+    def test_secure_av_inclusive(self):
+        expression = re.compile(rfc._SECURE_AV_REGEX)
+        self.assertTrue(
+            bool(expression.fullmatch("Secure")),
+            msg="Good Secure AV 'Secure' not matched by regex")
+
+    def test_secure_av_exclusive(self):
+        expression = re.compile(rfc._SECURE_AV_REGEX)
+        bad_examples = [
+            "secure",
+            "Secure=True",
+            "Secure=",
+            "",
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Secure AV '{m}' matched by regex".format(m=bad_example))
+
+    def test_http_only_av_inclusive(self):
+        expression = re.compile(rfc._HTTP_ONLY_AV_REGEX)
+        self.assertTrue(
+            bool(expression.fullmatch("HttpOnly")),
+            msg="Good HttpOnly AV 'HttpOnly' not matched by regex")
+
+    def test_httponly_av_exclusive(self):
+        expression = re.compile(rfc._HTTP_ONLY_AV_REGEX)
+        bad_examples = [
+            "Httponly",
+            "HttpOnly=True",
+            "HttpOnly=",
+            "",
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad HttpOnly AV '{m}' matched by regex".format(m=bad_example))
+
+    def test_extension_av_inclusive(self):
+        expression = re.compile(rfc._EXTENTION_AV_REGEX)
+        good_examples = [
+            "some random text",
+            "any$%^&thing)(* but not ctl chars or semi:"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good Extension AV '{m}' not matched by regex".format(m=good_example))
+
+    def test_extension_av_exclusive(self):
+        expression = re.compile(rfc._EXTENTION_AV_REGEX)
+        bad_examples = [
+            "has a;",
+        ] + [chr(x) for x in range(0, 32)] + [chr(127)]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad Extension AV '{m}' matched by regex".format(m=bad_example))
+
+
+
+
 
 
 
