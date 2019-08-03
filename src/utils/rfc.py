055af44882cadf0d526e308c7b075076c5195539
cbefus <crbefus@gmail.com>
2019-08-02 21:40:14 -0500

    started adding rfc header cookie regexs with tests.

---
+++ src/utils/rfc.py
@@ -0,0 +1,74 @@
+"""
+These are constants and expressions that match RFC guidelines laid out in the following docs
+General: https://www.ietf.org/rfc/rfc2616.txt  (sec 2.2)
+Dates: https://www.ietf.org/rfc/rfc2616.txt (sec 3.3)
+Domains: https://www.ietf.org/rfc/rfc1034.txt (sec 3.5) and https://www.ietf.org/rfc/rfc1123.txt (sec 2.1)
+Cookies: https://www.ietf.org/rfc/rfc6265.txt (sec 4)
+
+"""
+
+### General
+
+_CONTROL_CHARS = r"\x00-\x1F\x7F"
+_SPACE_CHAR = r"\x20"
+_TAB_CHAR = r"\x09"
+_SEPARATOR_CHARS = r"\x28\x29\x3C\x3E\x40\x2C\x3B\x3A\x5C\x22\x2F\x5B\x5D\x3F\x3D\x7B\x7D" + _SPACE_CHAR + "" + _TAB_CHAR
+_TOKEN_REGEX = r"^([^" + _CONTROL_CHARS + _SEPARATOR_CHARS + "])+$"
+
+### Dates (GMT Only)
+"""
+Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
+Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850, obsoleted by RFC 1036
+Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+"""
+_1_DIGIT_REGEX = r"([0-9]){1}"
+_2_DIGIT_REGEX = r"([0-9]){2}"
+_4_DIGIT_REGEX = r"([0-9]){4,4}"
+_TIME_ZONE_SUFFIX = r"GMT"
+_TIME_REGEX = r"(" + _2_DIGIT_REGEX + ":" + _2_DIGIT_REGEX + ":" + _2_DIGIT_REGEX + ")"  # note the dont force reasonable numbers in RFC
+_MONTH_REGEX = r"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec){1}"
+_WEEKDAY_REGEX = r"(Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday){1}"
+_WKDAY_REGEX = r"(Mon|Tue|Wed|Thu|Fri|Sat|Sun){1}"
+_ASCTIME_DATE_REGEX = r"(" + _MONTH_REGEX + _SPACE_CHAR + "(" + _2_DIGIT_REGEX + "|(" + _SPACE_CHAR + _1_DIGIT_REGEX + "){1}){1}" + "){1}"
+_RFC850_DATE_REGEX = r"(" + _2_DIGIT_REGEX + "-" + _MONTH_REGEX + "-" + _2_DIGIT_REGEX + "){1}"
+_RFC1123_DATE_REGEX = r"(" + _2_DIGIT_REGEX + _SPACE_CHAR + _MONTH_REGEX + _SPACE_CHAR + _4_DIGIT_REGEX + "){1}"
+_ASCTIME_DATETIME_REGEX = r"(" + _WKDAY_REGEX + _SPACE_CHAR + _ASCTIME_DATE_REGEX + _SPACE_CHAR + _TIME_REGEX + _SPACE_CHAR + _4_DIGIT_REGEX + "){1}"
+_RFC850_DATETIME_REGEX = r"(" + _WEEKDAY_REGEX + "," + _SPACE_CHAR + _RFC850_DATE_REGEX + _SPACE_CHAR + _TIME_REGEX + _SPACE_CHAR + _TIME_ZONE_SUFFIX + "){1}"
+_RFC1123_DATETIME_REGEX = r"(" + _WKDAY_REGEX + "," + _SPACE_CHAR + _RFC1123_DATE_REGEX + _SPACE_CHAR + _TIME_REGEX + _SPACE_CHAR + _TIME_ZONE_SUFFIX + "){1}"
+_HTTP_DATE_REGEX = r"^(" + _RFC1123_DATETIME_REGEX + "|" + _RFC850_DATETIME_REGEX + "|" + _ASCTIME_DATETIME_REGEX + "){1}$"
+
+
+### Domains
+
+_LETTERS_REGEX = r"([A-Za-z])"
+_LETTERS_DIGITS_REGEX = r"([A-Za-z0-9])"
+_LETTER_DIGITS_HYPHENS_REGEX = r"([A-Za-z0-9\-])"
+_LABEL_REGEX = r"((" + _LETTERS_REGEX + "+(" + _LETTER_DIGITS_HYPHENS_REGEX + "*" + _LETTERS_DIGITS_REGEX + "+)*){1,63}"
+_SUBDOMAIN_REGEX = r"(" + _LABEL_REGEX + "(." + _LABEL_REGEX + ")*){1}"
+
+_IPV4_IP_REGEX = r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?){1}"
+_IPV6_IP_REGEX = r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])){1}"
+_IP_REGEX = r"(" + _IPV4_IP_REGEX + "|" + _IPV6_IP_REGEX + "){1}"
+
+_DOMAIN_REGEX = r"(" + _SUBDOMAIN_REGEX + "|" + _IP_REGEX + "){1}"
+
+### Cookies
+
+_COOKIE_NAME_REGEX = _TOKEN_REGEX
+_COOKIE_OCTET_REGEX = r"([\x21\x23-\x2B\x2D-\x3A\x3C-\x5B\x5D-\x7E]){1}"
+_COOKIE_VALUE_REGEX = r"((" + _COOKIE_OCTET_REGEX + ")+|(\"(" + _COOKIE_OCTET_REGEX + ")+\")){1}"
+_EXPIRES_AV_REGEX = r"(Expires=" + _HTTP_DATE_REGEX + "){1}"
+_MAX_AGE_AV_REGEX = r"(Max-Age=([1-9])+){1}"
+_DOMAIN_AV_REGEX = r"(Domain=" + _DOMAIN_REGEX + "){1}"
+_PATH_AV_REGEX = r"(Path=([^" + _CONTROL_CHARS + ";])+){1}"
+_SECURE_AV_REGEX = r"(Secure){1}"
+_HTTP_ONLY_AV_REGEX = r"(HttpOnly){1}"
+_EXTENTION_AV_REGEX = r"(([^" + _CONTROL_CHARS + ";])+){1}"
+_COOKIE_AV_REGEX = r"(" + _EXPIRES_AV_REGEX + "|" + _MAX_AGE_AV_REGEX + "|" + _DOMAIN_AV_REGEX + "|" + _PATH_AV_REGEX + "|" + _SECURE_AV_REGEX + "|" + _HTTP_ONLY_AV_REGEX + "|" + _EXTENTION_AV_REGEX + ")*"
+_COOKIE_PAIR_REGEX = r"(" + _COOKIE_NAME_REGEX + "=" + _COOKIE_VALUE_REGEX + "){1}"
+_SET_COOKIE_STRING_REGEX = r"(" + _COOKIE_PAIR_REGEX + "(;" + _SPACE_CHAR + _COOKIE_AV_REGEX + ")*" + "){1}"
+_SET_COOKIE_HEADER_REGEX = r"(Set-Cookie:" + _SPACE_CHAR + _SET_COOKIE_STRING_REGEX + "){1}"
+
+
+
+
---
+++ tests/utils/test_rfc.py
@@ -0,0 +1,385 @@
+import unittest
+import re
+
+from src.utils import rfc
+
+
+class TestGeneral(unittest.TestCase):
+
+    def test_control_chars_inclusive(self):
+        expression = re.compile(r"["+rfc._CONTROL_CHARS+"]")
+        control_character_ordinals = frozenset([127]+[x for x in range(0,32)])
+        for control_character_ordinal in control_character_ordinals:
+            value_to_test = chr(control_character_ordinal)
+            self.assertTrue(
+                bool(expression.fullmatch(value_to_test)),
+                msg="Character for ord '{o}' was not included".format(o=control_character_ordinal))
+
+    def test_control_chars_exclusive(self):
+        expression = re.compile(r"[" + rfc._CONTROL_CHARS + "]")
+        control_character_ordinals = frozenset([x for x in range(32, 127)])
+        for control_character_ordinal in control_character_ordinals:
+            value_to_test = chr(control_character_ordinal)
+            self.assertFalse(
+                bool(expression.fullmatch(value_to_test)),
+                msg="Character for ord '{o}' was included".format(o=control_character_ordinal))
+
+    def test_space_char(self):
+        expression = re.compile(r"[" + rfc._SPACE_CHAR + "]")
+        self.assertTrue(bool(expression.fullmatch(" ")), msg="Space was not found")
+
+    def test_tab_char(self):
+        expression = re.compile(r"[" + rfc._TAB_CHAR + "]")
+        self.assertTrue(bool(expression.fullmatch("\t")), msg="Tab was not found")
+
+    def test_separators_inclusive(self):
+        expression = re.compile(r"[" + rfc._SEPARATOR_CHARS + "]")
+        separators = ["(", ")", "<", ">", "@", ",", ";", ":", "\\", "\"", "/", "[", "]", "?", "=", "{", "}", " ", "\t"]
+        for separator in separators:
+            self.assertTrue(
+                bool(expression.fullmatch(separator)),
+                msg="Separator '{s}' was not found".format(s=separator))
+
+    def test_token_inclsive(self):
+        expression = re.compile(rfc._TOKEN_REGEX)
+        valid_tokens = [
+            "a",
+            "abcd",
+            "abraca+dab_ra-goes",
+            "other_syms!#$%^&*_-+|'~`."
+        ]
+        for valid_token in valid_tokens:
+            self.assertTrue(
+                bool(expression.fullmatch(valid_token)),
+                msg="Valid Token '{t}' was flagged".format(t=valid_token))
+
+    def test_token_exclusive(self):
+        expression = re.compile(rfc._TOKEN_REGEX)
+        invalid_tokens = [chr(127)]+[chr(x) for x in range(0,32)]
+        invalid_tokens += ["(", ")", "<", ">", "@", ",", ";", ":", "\\", "\"", "/", "[", "]", "?", "=", "{", "}", " ", "\t"]
+        for invalid_token in invalid_tokens:
+            self.assertFalse(
+                bool(expression.fullmatch("abc"+invalid_token+"def")),
+                msg="Invalid Token 'abc{t}def' was not flagged".format(t=invalid_token))
+
+
+class TestDates(unittest.TestCase):
+
+    def test_1_digit(self):
+        expression = re.compile(rfc._1_DIGIT_REGEX)
+        self.assertTrue(bool(expression.fullmatch("3")))
+        self.assertFalse(bool(expression.fullmatch("34")))
+        self.assertFalse(bool(expression.fullmatch("a")))
+        self.assertFalse(bool(expression.fullmatch("8a")))
+
+    def test_2_digit(self):
+        expression = re.compile(rfc._2_DIGIT_REGEX)
+        self.assertTrue(bool(expression.fullmatch("38")))
+        self.assertFalse(bool(expression.fullmatch("4")))
+        self.assertFalse(bool(expression.fullmatch("a")))
+        self.assertFalse(bool(expression.fullmatch("48a")))
+
+    def test_4_digit(self):
+        expression = re.compile(rfc._4_DIGIT_REGEX)
+        self.assertTrue(bool(expression.fullmatch("3878")))
+        self.assertFalse(bool(expression.fullmatch("432")))
+        self.assertFalse(bool(expression.fullmatch("43234")))
+        self.assertFalse(bool(expression.fullmatch("a")))
+        self.assertFalse(bool(expression.fullmatch("2348a")))
+
+    def test_time_regex_inclusive(self):
+        expression = re.compile(rfc._TIME_REGEX)
+        self.assertTrue(bool(expression.fullmatch("12:22:06")))
+        self.assertTrue(bool(expression.fullmatch("00:00:00")))
+
+    def test_time_regex_exclusive(self):
+        expression = re.compile(rfc._TIME_REGEX)
+        bad_times = [
+            "a",
+            "one o' clock",
+            "9:52",
+            "9:52am",
+            "6:34:27",
+            "19:4:27",
+            "19:34:7",
+            "12:34:27pm"
+        ]
+        for bad_time in bad_times:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_time)),
+                msg="Bad Time '{t}' was not flagged".format(t=bad_time))
+
+    def test_month_inclusive(self):
+        expression = re.compile(rfc._MONTH_REGEX)
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        for month in months:
+            self.assertTrue(
+                bool(expression.fullmatch(month)),
+                msg="Month '{m}' was not included in regex.".format(m=month))
+
+    def test_month_exclusive(self):
+        expression = re.compile(rfc._MONTH_REGEX)
+        bad_months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+            "Abc",
+            "Ja",
+            ""
+        ]
+        for bad_month in bad_months:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_month)),
+                msg="Bad Month Encoding '{m}' was matched and shouldn't have been".format(m=bad_month))
+
+    def test_weekday_inclusive(self):
+        expression = re.compile(rfc._WEEKDAY_REGEX)
+        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        for weekday in weekdays:
+            self.assertTrue(
+                bool(expression.fullmatch(weekday)),
+                msg="Weekday '{w}' was not included in regex.".format(w=weekday))
+
+    def test_weekday_exclusive(self):
+        expression = re.compile(rfc._WEEKDAY_REGEX)
+        bad_weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Abc", "Tu", ""]
+        for bad_weekday in bad_weekdays:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_weekday)),
+                msg="Bad Weekday Encoding '{m}' was matched and shouldn't have been".format(m=bad_weekday))
+
+    def test_wkday_inclusive(self):
+        expression = re.compile(rfc._WKDAY_REGEX)
+        weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        for weekday in weekdays:
+            self.assertTrue(
+                bool(expression.fullmatch(weekday)),
+                msg="Weekday '{w}' was not included in regex.".format(w=weekday))
+
+    def test_wkday_exclusive(self):
+        expression = re.compile(rfc._WKDAY_REGEX)
+        bad_weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Abc", "Tu", ""]
+        for bad_weekday in bad_weekdays:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_weekday)),
+                msg="Bad Weekday Encoding '{m}' was matched and shouldn't have been".format(m=bad_weekday))
+
+    def test_asctime_date_inclusive(self):
+        expression = re.compile(rfc._ASCTIME_DATE_REGEX)
+        good_dates = ["Jan 21", "Feb  3"]
+        for good_date in good_dates:
+            self.assertTrue(
+                bool(expression.fullmatch(good_date)),
+                msg="Good date format '{d}' didnt match regex".format(d=good_date))
+
+    def test_asctime_date_exclusive(self):
+        expression = re.compile(rfc._ASCTIME_DATE_REGEX)
+        bad_dates = [
+            "Feb 3",
+            "January 3",
+            "Feb 3 1997",
+            "January 3 1997",
+            "Something else",
+            "07/23",
+            "07/23/97",
+            "23-Jan-98",
+            "23-Jan-1998",
+            "23-07-1998",
+            "23-07-98",
+            "31 Oct 1982"
+        ]
+        for bad_date in bad_dates:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_date)),
+                msg="Bad date '{d}' didn't match regex".format(d=bad_date))
+
+    def test_rfc850_date_inclusive(self):
+        expression = re.compile(rfc._RFC850_DATE_REGEX)
+        good_dates = ["31-Oct-82", "01-Jan-23"]
+        for good_date in good_dates:
+            self.assertTrue(
+                bool(expression.fullmatch(good_date)),
+                msg="Good date format '{d}' didnt match regex".format(d=good_date))
+
+    def test_rfc850_date_exclusive(self):
+        expression = re.compile(rfc._RFC850_DATE_REGEX)
+        bad_dates = [
+            "Jan 21",
+            "Feb  3",
+            "Feb 3",
+            "January 3",
+            "Feb 3 1997",
+            "January 3 1997",
+            "Something else",
+            "07/23",
+            "07/23/97",
+            "23-Jan-1998",
+            "23-07-1998",
+            "23-07-98",
+            "31 Oct 1982"
+        ]
+        for bad_date in bad_dates:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_date)),
+                msg="Bad date '{d}' didn't match regex".format(d=bad_date))
+
+    def test_rfc1123_date_inclusive(self):
+        expression = re.compile(rfc._RFC1123_DATE_REGEX)
+        good_dates = ["31 Oct 1982", "01 Jan 2023"]
+        for good_date in good_dates:
+            self.assertTrue(
+                bool(expression.fullmatch(good_date)),
+                msg="Good date format '{d}' didnt match regex".format(d=good_date))
+
+    def test_rfc1123_date_exclusive(self):
+        expression = re.compile(rfc._RFC1123_DATE_REGEX)
+        bad_dates = [
+            "Jan 21",
+            "Feb  3",
+            "Feb 3",
+            "January 3",
+            "Feb 3 1997",
+            "January 3 1997",
+            "Something else",
+            "07/23",
+            "07/23/97",
+            "23-Jan-1998",
+            "23-07-1998",
+            "23-07-98",
+            "31-Oct-82"
+        ]
+        for bad_date in bad_dates:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_date)),
+                msg="Bad date '{d}' didn't match regex".format(d=bad_date))
+
+    def test_asc_datetime_inclusive(self):
+        expression = re.compile(rfc._ASCTIME_DATETIME_REGEX)
+        good_examples = [
+            "Mon Jan  3 03:21:45 2045",
+            "Thu Oct 31 20:34:22 1982"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good asctime datetime '{d}' didn't match regex".format(d=good_example))
+
+    def test_asc_datetime_exclusive(self):
+        expression = re.compile(rfc._ASCTIME_DATETIME_REGEX)
+        bad_examples = [
+            "Mon, 31 Oct 1982 03:21:45 GMT",
+            "Thu, 01 Feb 2045 20:34:22 GMT",
+            "Monday, 31-Oct-82 03:21:45 GMT",
+            "Thursday, 01-Feb-45 20:34:22 GMT",
+            "Something else"
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad asctime datetime '{d}' matched regex".format(d=bad_example))
+
+    def test_rfc850_datetime_inclusive(self):
+        expression = re.compile(rfc._RFC850_DATETIME_REGEX)
+        good_examples = [
+            "Monday, 31-Oct-82 03:21:45 GMT",
+            "Thursday, 01-Feb-45 20:34:22 GMT"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good rfc850 datetime '{d}' didn't match regex".format(d=good_example))
+
+    def test_rfc850_datetime_exclusive(self):
+        expression = re.compile(rfc._RFC850_DATETIME_REGEX)
+        bad_examples = [
+            "Mon Jan  3 03:21:45 2045",
+            "Thu Oct 31 20:34:22 1982",
+            "Monday, 31-Oct-82 03:21:45",
+            "Monday 31-Oct-82 03:21:45 GMT",
+            "Mon, 31 Oct 1982 03:21:45 GMT",
+            "Thu, 01 Feb 2045 20:34:22 GMT",
+            "Something else"
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad rfc850 datetime '{d}' matched regex".format(d=bad_example))
+
+    def test_rfc1123_datetime_inclusive(self):
+        expression = re.compile(rfc._RFC1123_DATETIME_REGEX)
+        good_examples = [
+            "Mon, 31 Oct 1982 03:21:45 GMT",
+            "Thu, 01 Feb 2045 20:34:22 GMT"
+        ]
+        for good_example in good_examples:
+            self.assertTrue(
+                bool(expression.fullmatch(good_example)),
+                msg="Good rfc1123 datetime '{d}' didn't match regex".format(d=good_example))
+
+    def test_rfc1123_datetime_exclusive(self):
+        expression = re.compile(rfc._RFC1123_DATETIME_REGEX)
+        bad_examples = [
+            "Mon Jan  3 03:21:45 2045",
+            "Thu Oct 31 20:34:22 1982",
+            "Mon 31 Oct 1982 03:21:45 GMT",
+            "Thu, 01 Feb 2045 20:34:22",
+            "Thu, 01 Feb  2045 20:34:22 GMT",
+            "Monday, 31-Oct-82 03:21:45",
+            "Monday 31-Oct-82 03:21:45 GMT",
+            "Something else"
+        ]
+        for bad_example in bad_examples:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_example)),
+                msg="Bad rfc850 datetime '{d}' matched regex".format(d=bad_example))
+
+    def test_http_date_inclusive(self):
+        expression = re.compile(rfc._HTTP_DATE_REGEX)
+        good_dates = [
+            "Mon Jan  3 03:21:45 2045",
+            "Thu Oct 31 20:34:22 1982",
+            "Monday, 31-Oct-82 03:21:45 GMT",
+            "Thursday, 01-Feb-45 20:34:22 GMT",
+            "Mon, 31 Oct 1982 03:21:45 GMT",
+            "Thu, 01 Feb 2045 20:34:22 GMT"
+        ]
+        for good_date in good_dates:
+            self.assertTrue(
+                bool(expression.fullmatch(good_date)),
+                msg="Good HTTP Date format '{d}' wasn't matched by regex".format(d=good_date))
+
+    def test_http_date_exclusive(self):
+        expression = re.compile(rfc._HTTP_DATE_REGEX)
+        bad_dates = [
+            "Something else",
+            "Mon Jan 3 03:21:45 2045",
+            "Mon, Jan  3 03:21:45 2045",
+            "Mon Jan  3 03:1:45 2045",
+            "Mon Jan  3 03:01:45 45",
+            "Mon Jan  3 03:01:45 2045 GMT",
+            "Thu Oct  31 20:34:22 1982",
+            "Monday 31-Oct-82 03:21:45 GMT",
+            "Monday, 31-Oct-82 03:21:45",
+            "Monday, 31-Oct-1982 03:21:45 GMT",
+            "Monday, 6-Oct-82 03:21:45 GMT",
+            "Mon 31 Oct 1982 03:21:45 GMT",
+            "Mon, 31 Oct 1982 03:21:45",
+            "Mon, 31 Oct 82 03:21:45 GMT",
+            "Mon, 6 Oct 1982 03:21:45 GMT"
+        ]
+        for bad_date in bad_dates:
+            self.assertFalse(
+                bool(expression.fullmatch(bad_date)),
+                msg="Bad HTTP Date format '{d}' matched by regex".format(d=bad_date))
+
+
+
+
