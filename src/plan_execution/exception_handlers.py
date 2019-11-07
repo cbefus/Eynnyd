07c70e29ca93526e1bc25037e71bfc497ab78100
cbefus <crbefus@gmail.com>
2019-11-06 21:41:39 -0600

    Tested exception handlers class and improved naming.

--- src/plan_execution/exception_handlers.py
+++ src/plan_execution/exception_handlers.py
@@ -1,11 +1,6 @@
-import inspect
 import logging
-from http import HTTPStatus
 
-from src.exceptions import ExceptionHandlingRegisterException, NoGenericExceptionHandlerRegistered, \
-    RouteNotFoundException, CallbackIncorrectNumberOfParametersException, NonCallableExceptionHandlerException, \
-    InvalidCookieHeaderException
-from src.response import ResponseBuilder
+from src.exceptions import NoGenericExceptionHandlerRegistered
 
 LOG = logging.getLogger("exception_handlers")
 
@@ -19,19 +14,24 @@ class ExceptionHandlers:
         self._pre_response_error_handlers = pre_response_error_handlers
         self._post_response_error_handlers = post_response_error_handlers
 
-    def handle_pre_response_error(self, exc, request):
-        return ExceptionHandlers._get_handler_for_exception(exc, self._pre_response_error_handlers)(exc, request)
-
-    def handle_post_response_error(self, exc, request, response):
+    def handle_pre_response_error(self, thrown_exception, request):
         return ExceptionHandlers\
-            ._get_handler_for_exception(exc, self._post_response_error_handlers)(exc, request, response)
+            ._get_handler_for_exception(thrown_exception, self._pre_response_error_handlers)(thrown_exception, request)
+
+    def handle_post_response_error(self, thrown_exception, request, response):
+        return ExceptionHandlers\
+            ._get_handler_for_exception(
+                thrown_exception,
+                self._post_response_error_handlers)(thrown_exception, request, response)
 
     @staticmethod
-    def _get_handler_for_exception(exc, error_handlers):
-        for k, v in error_handlers:
-            if isinstance(exc, k):
-                return v
-        raise NoGenericExceptionHandlerRegistered("No exception handler registered for even generic exceptions.", exc)
+    def _get_handler_for_exception(thrown_exception, error_handlers):
+        for registered_exception, registered_handler in error_handlers:
+            if isinstance(thrown_exception, registered_exception):
+                return registered_handler
+        raise NoGenericExceptionHandlerRegistered(
+            "No exception handler registered for even generic exceptions.",
+            thrown_exception)
 
 
 
---
+++ tests/plan_execution/test_exception_handlers.py
@@ -0,0 +1,123 @@
+import unittest
+
+from src.plan_execution.exception_handlers import ExceptionHandlers
+from src.exceptions import NoGenericExceptionHandlerRegistered
+
+
+class TestExceptionHandlers(unittest.TestCase):
+
+    def test_no_matching_pre_exception_handler_raises(self):
+
+        class CustomTestException(Exception):
+            pass
+
+        class CustomRegisterException(Exception):
+            pass
+
+        class SpyFakeExceptionHandler:
+            def __init__(self):
+                self._call_count = 0
+
+            @property
+            def call_count(self):
+                return self._call_count
+
+            def test_fake_handler(self, exc, request):
+                self._call_count += 1
+
+        fake_handler = SpyFakeExceptionHandler()
+        exception_handlers = ExceptionHandlers([], [(CustomRegisterException, fake_handler.test_fake_handler)])
+        with self.assertRaises(NoGenericExceptionHandlerRegistered):
+            exception_handlers.handle_pre_response_error(CustomTestException(), None)
+        self.assertEqual(0, fake_handler.call_count)
+
+    def test_no_matching_post_exception_handler_raises(self):
+
+        class CustomTestException(Exception):
+            pass
+
+        class CustomRegisterException(Exception):
+            pass
+
+        class SpyFakeExceptionHandler:
+            def __init__(self):
+                self._call_count = 0
+
+            @property
+            def call_count(self):
+                return self._call_count
+
+            def test_fake_handler(self, exc, request, response):
+                self._call_count += 1
+
+        fake_handler = SpyFakeExceptionHandler()
+        exception_handlers = ExceptionHandlers([], [(CustomRegisterException, fake_handler.test_fake_handler)])
+        with self.assertRaises(NoGenericExceptionHandlerRegistered):
+            exception_handlers.handle_post_response_error(CustomTestException(), None, None)
+        self.assertEqual(0, fake_handler.call_count)
+
+    def test_matching_pre_exception_handler_is_called(self):
+
+        class CustomRegisterException(Exception):
+            pass
+
+        class SpyFakeExceptionHandler:
+            def __init__(self):
+                self._pre_call_count = 0
+                self._post_call_count = 0
+
+            @property
+            def pre_call_count(self):
+                return self._pre_call_count
+
+            @property
+            def post_call_count(self):
+                return self._post_call_count
+
+            def test_pre_fake_handler(self, exc, request):
+                self._pre_call_count += 1
+
+            def test_post_fake_handler(self, exc, request, response):
+                self._post_call_count += 1
+
+        fake_handler = SpyFakeExceptionHandler()
+        exception_handlers = \
+            ExceptionHandlers(
+                [(CustomRegisterException, fake_handler.test_pre_fake_handler)],
+                [(CustomRegisterException, fake_handler.test_post_fake_handler)])
+        exception_handlers.handle_pre_response_error(CustomRegisterException(), None)
+        self.assertEqual(1, fake_handler.pre_call_count)
+        self.assertEqual(0, fake_handler.post_call_count)
+
+    def test_matching_post_exception_handler_is_called(self):
+
+        class CustomRegisterException(Exception):
+            pass
+
+        class SpyFakeExceptionHandler:
+            def __init__(self):
+                self._pre_call_count = 0
+                self._post_call_count = 0
+
+            @property
+            def pre_call_count(self):
+                return self._pre_call_count
+
+            @property
+            def post_call_count(self):
+                return self._post_call_count
+
+            def test_pre_fake_handler(self, exc, request):
+                self._pre_call_count += 1
+
+            def test_post_fake_handler(self, exc, request, response):
+                self._post_call_count += 1
+
+        fake_handler = SpyFakeExceptionHandler()
+        exception_handlers = \
+            ExceptionHandlers(
+                [(CustomRegisterException, fake_handler.test_pre_fake_handler)],
+                [(CustomRegisterException, fake_handler.test_post_fake_handler)])
+        exception_handlers.handle_post_response_error(CustomRegisterException(), None, None)
+        self.assertEqual(0, fake_handler.pre_call_count)
+        self.assertEqual(1, fake_handler.post_call_count)
