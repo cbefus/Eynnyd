fff6ca314bf04dc5e0d7061b6e60d53b268c37f5
cbefus <crbefus@gmail.com>
2019-11-07 20:34:44 -0600

    Added tests around route tree building.

---
+++ tests/routing/test_route_tree_builder.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from src.exceptions import DuplicateHandlerRoutesException
+from src.routing.route_tree_builder import RouteTeeBuilder
+
+
+class TestRouteTeeBuilder(TestCase):
+
+    def test_cant_add_duplicate_handler_routes(self):
+        def fake_handler(request):
+            pass
+
+        def another_fake_handler(request):
+            pass
+
+        builder = RouteTeeBuilder()
+        builder.add_handler("GET", ["foo", "bar"], fake_handler)
+        with self.assertRaises(DuplicateHandlerRoutesException):
+            builder.add_handler("GET", ["foo", "bar"], another_fake_handler)
