21ca75916fd1636c5a9497a26d2dc892afb8cd29
cbefus <crbefus@gmail.com>
2019-11-08 12:55:47 -0600

    Added tests around wsgi server error response.

---
+++ tests/wsgi/test_raw_wsgi_server_error_response.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from src.wsgi.raw_wsgi_server_error_response import RawWSGIServerErrorResponse
+
+
+class TestRawWSGIServerErrorResponse(TestCase):
+
+    def test_server_error_response_properties(self):
+        response = RawWSGIServerErrorResponse()
+        self.assertEqual("500 INTERNAL_SERVER_ERROR", response.status)
+        self.assertListEqual([], response.headers)
+        self.assertListEqual(["500 Internal Server Error".encode("utf-8")], response.body)
+
+
+
