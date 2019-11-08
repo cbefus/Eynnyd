e38a515bd7812307bde9e3ce9635a6ea7552e44e
cbefus <crbefus@gmail.com>
2019-11-07 19:23:08 -0600

    Renamed response cookie builder test module and added test around the request cookie.

---
+++ tests/utils/cookies/test_request_cookie.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.utils.cookies.request_cookie import RequestCookie
+
+
+class TestRequestCookie(unittest.TestCase):
+
+    def test_properties(self):
+        cookie = RequestCookie("foo", "bar")
+        self.assertEqual("foo", cookie.name)
+        self.assertEqual("bar", cookie.value)
+
