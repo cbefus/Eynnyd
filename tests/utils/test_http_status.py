5127b29fc5fa7c2c1f3d0e861320e1854b07322c
cbefus <crbefus@gmail.com>
2019-11-07 21:17:19 -0600

    Added tests around http status.

---
+++ tests/utils/test_http_status.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from src.utils.http_status import HTTPStatus
+
+
+class TestHTTPStatus(TestCase):
+
+    def test_http_status_str(self):
+        status = HTTPStatus(400, "bad request", "generically bad request")
+        self.assertEqual("400 bad request", str(status))
+
+    def test_http_status_wsgi_format(self):
+        status = HTTPStatus(400, "bad request", "generically bad request")
+        self.assertEqual("400 bad request", status.wsgi_format)
+
+    def test_http_status_properties(self):
+        status = HTTPStatus(400, "bad request", "generically bad request")
+        self.assertEqual(400, status.code)
+        self.assertEqual("bad request", status.phrase)
+        self.assertEqual("generically bad request", status.description)
