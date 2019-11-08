0f4e4f224cb2274af51343fbed6413cef98c13a1
cbefus <crbefus@gmail.com>
2019-11-08 11:42:09 -0600

    Added tests around the http status factory.

---
+++ tests/utils/test_http_status_factory.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from http import HTTPStatus as HTTPLibStatus
+
+from src.exceptions import InvalidHTTPStatusException
+from src.utils.http_status import HTTPStatus
+from src.utils.http_status_factory import HTTPStatusFactory
+
+class TestHTTPStatusFactory(TestCase):
+
+    def test_build_from_eynnyd_http_status(self):
+        status = HTTPStatus(200, "ok")
+        self._assertSameStatus(status, HTTPStatusFactory.create(status))
+
+    def test_build_from_http_lib_status(self):
+        self._assertSameStatus(HTTPStatus(200, "OK"), HTTPStatusFactory.create(HTTPLibStatus.OK))
+
+    def test_build_from_number(self):
+        self._assertSameStatus(HTTPStatus(200, "OK"), HTTPStatusFactory.create(200))
+
+    def test_build_from_custom(self):
+        self._assertSameStatus(HTTPStatus(602, "Custom Status"), HTTPStatusFactory.create(602))
+
+    def test_raises_on_invalid_type(self):
+        with self.assertRaises(InvalidHTTPStatusException):
+            HTTPStatusFactory.create("OK")
+
+    def _assertSameStatus(self, expected, actual):
+        self.assertEqual(expected.code, actual.code)
+        self.assertEqual(expected.phrase, actual.phrase)
+        self.assertEqual(expected.description, actual.description)
+
+
+
