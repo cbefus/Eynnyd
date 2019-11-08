649e9818b0cb7dd9f6428c5ac21edb2fc066b0c1
cbefus <crbefus@gmail.com>
2019-11-08 12:51:17 -0600

    Added tests for stream reader factory

---
+++ tests/wsgi/test_stream_reader_factory.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from src.wsgi.stream_reader_factory import StreamReaderFactory
+from src.wsgi.closeable_stream_iterator import CloseableStreamIterator
+
+
+class TestStreamReaderFactory(TestCase):
+
+    def test_create_reader_from_passed_in(self):
+        self.assertEqual("foobar", StreamReaderFactory.create_reader("foobar"))
+
+    def test_create_reader_from_none(self):
+        self.assertEqual(CloseableStreamIterator, StreamReaderFactory.create_reader(None))
+
